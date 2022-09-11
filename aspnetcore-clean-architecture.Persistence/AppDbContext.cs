1946a225858c08a8976a7aa2a547987e5455c064
agha <test@gmail.com>
2022-09-11 22:38:46 +0200

    added entity model and dbcontext

---
+++ aspnetcore-clean-architecture.Persistence/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using aspnetcore_clean_architecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnetcore_clean_architecture.Persistence
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                 new User
+                 {
+                     Id = Guid.NewGuid(),
+                     Name = "John",
+                     Surname = "Doe",
+                     DateCreated = DateTime.Now,
+                     DateModified = DateTime.Now,
+                 }
+            );
+        }
+    }
+}
